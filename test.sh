f5af863cf41308e9dfd803512756914d8fe3fcfd
Jeril Sebastian <seb988@gmail.com>
2017-12-27 21:29:39 +0530

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-git filter-branch --env-filter \
-    'if [ $GIT_COMMIT = d5118d58c75d41967d89ad8264b224382e0783d1 ]
-     then
-         export GIT_AUTHOR_DATE="Thu Aug 24 22:25:08 2017 +0530"
-         export GIT_COMMITTER_DATE="Thu Aug 24 22:15:08 2017 +0530"
-     fi'
-
-rm -rf .git/refs/original
